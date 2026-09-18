9496529328e931853e7c8c47515a98892ce19dfa
Christopher Edward Dawn <cedtwo@tutanota.com>
2026-09-18 16:23:55 +0800

    Add README.md to tilebound_solid and Cargo.toml

--- crates/solid/src/lib.rs
+++ crates/solid/src/lib.rs
@@ -1,3 +1,39 @@
+//! # Tilebound-Solid
+//!
+//! A small boolean/bit tilemap collision libary.
+//!
+//! ## Summary
+//!
+//! `tilebound_solid` is a simle implementation of [`tilebound`] supporting arrays of simple
+//! solid/empty tiles. It offers the following features:
+//! - Axis seperate *sweep* displacement that only checks tiles on-demand (when moving into a new
+//! tile),
+//! - Attach/detach behaviour (for asserting collisions) when moving on/off surfaces,
+//! - Tile range inspection on collision (eg. Check where the first solid tile is when colliding
+//! with a ledge),
+//! - Perfect tile-bound alignment (no fractional displacement for an edge on a tile bound),
+//! - Support for `bool`, `u8` (0 or 1) and `bitvec` backed tile arrays.
+//!
+//! A large number of these features are provided by the implementing library, [`tilebound`].
+//!
+//! ## features
+//!
+//! `tilebound_solid` supports the following features:
+//!
+//! Feature | Description | Exposed Type(s)
+//! ---|---|---
+//! `arraymap` | Exports tilemap implementations built on `std` types. | [`ArrayMap`](crate::prelude::ArrayMap), [`VecMap`](crate::prelude::VecMap)
+//! `bitmap` | Exports a `bitvec` backed tilemap (where each tile represents one bit). | [`BitMap`](crate::prelude::BitMap)
+//!
+//! ## Example
+//!
+//! A minimal example is provided using `macroquad` as a backend. `bevy` uses a different axis
+//! orientation so variables will either need to be manually oriented, or another rendering backend
+//! (such as `bevy_framebuffer`) will be needed.
+//!
+//! ```bash
+//! cargo run --example minimal --features="bitmap"
+//! ```
 mod context;
 
 mod collision;
